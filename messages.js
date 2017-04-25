c2fce407048c229216e611b9bbe911a31c30cc35
Josef Petrak <josef.petrak@somemove.cz>
2017-04-25 14:48:43 +0200

    Storing messages to remote MongoDB

--- messages.js
+++ messages.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var dbClient = require('mongodb').MongoClient
+
 module.exports = function (context, request, response) {
 	if (request.method == 'POST') {
 		var content = ''
@@ -10,6 +12,14 @@ module.exports = function (context, request, response) {
 			var data = JSON.parse(content)
 
 			console.log(data)
+
+			// Expects MONGO_URL as secret parameter of WebTask
+			dbClient.connect(context.data.MONGO_URL, function (error, db) {
+				if (error) return done(error)
+
+				var collection = db.collection('messages')
+				collection.insert(data)
+			})
 
 			response.statusCode = 201
 			response.end()
